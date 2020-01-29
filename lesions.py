fe2a25cbdeebbd034bc40d402b0aff90910d4e9c
Bao Nguyen <baocon1320@gmail.com>
2020-01-30 00:26:33 +0700

    add py files

---
+++ lesions.py
@@ -0,0 +1,15 @@
+from imgaug import augmenters as iaa
+from mrcnn.config import Config
+from mrcnn import model as modellib
+from mrcnn import visualize
+from mrcnn import utils
+from imutils import paths
+import numpy as np
+import argparse
+import imutils
+import random
+import cv2
+import os
+
+if __name__ == "__main__":
+    print("okay")
